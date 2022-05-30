71a297a0be2cfdca52bce116f1959f3189c62648
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-05-30 15:49:53 +0300

    Modellerin Doğrulanması ve FluentValidation Kütüphanesi

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.BookOperations.CreateBook;
 using WebApi.BookOperations.DeleteBook;
@@ -66,7 +68,15 @@ namespace WebApi.AddControllers{
             try
             {
                 command.Model = newBook;
+                CreateBookCommandValidator validator = new CreateBookCommandValidator();
+                validator.ValidateAndThrow(command);
                 command.Handle();
+
+                // if(!result.IsValid)
+                //     foreach (var item in result.Errors)
+                //         Console.WriteLine("Özellik " + item.PropertyName + " - Error Message :" + item.ErrorMessage);
+                // else
+                //     command.Handle();
             }
             catch (Exception ex)
             {
@@ -100,6 +110,8 @@ namespace WebApi.AddControllers{
             {
                 DeleteBookCommand command = new DeleteBookCommand(_context);
                 command.BookId = id;
+                DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+                validator.ValidateAndThrow(command);
                 command.Handle();
             }
             catch (Exception ex)
